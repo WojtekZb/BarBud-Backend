4b98ec645b0c00f7fd56f5ee0bc99ea7c94cb211
WojtekZb <554101@student.fontys.nl>
2026-05-05 18:35:55 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/barbud/barbudbackend/BarBudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barbud.barbudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarBudBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarBudBackendApplication.class, args);
+    }
+
+}
