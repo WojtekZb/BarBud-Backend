00d1b1fbb4c6a8e655ea37907301083286c9c375
WojtekZb <554101@student.fontys.nl>
2026-05-05 23:08:07 +0200

    Added working Api health class

---
+++ src/main/java/com/barbud/barbudbackend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.barbud.barbudbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "BarBud API works!";
+    }
+}
--- src/test/java/com/barbud/barbudbackend/BarBudBackendApplicationTests.java
+++ src/test/java/com/barbud/barbudbackend/BarBudBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.barbud.barbudbackend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BarBudBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.barbud.barbudbackend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BarBudBackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
